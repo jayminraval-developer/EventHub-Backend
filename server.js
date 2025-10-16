e0e763e5bf34e552a97f71f8c93b9b8801b15b13
Jaymin Raval <jayminraval57@gmail.com>
2025-10-16 10:50:08 +0530

    modified server js

--- server.js
+++ server.js
@@ -9,26 +9,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
-
-// ✅ Setup CORS with allowed origin
-const allowedOrigins = [
-  "http://localhost:5173/", // dev
-  "https://event-hub-frontend-rosy.vercel.app/", 
-];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true, // optional — only needed if you use cookies or sessions
-  })
-);
-
+app.use(cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => res.send("EventHub Backend is running"));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
